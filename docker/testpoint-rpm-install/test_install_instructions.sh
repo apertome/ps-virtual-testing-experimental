7927d2a66e22e7f4675f320eaf92dcd3776a3d64
Michael Johnson <mj82@globalnoc.iu.edu>
2020-11-10 16:08:36 +0000

    Make output/varibles clearer

--- docker/testpoint-rpm-install/test_install_instructions.sh
+++ docker/testpoint-rpm-install/test_install_instructions.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/bash
 
-declare -a REPOS=("perfsonar-tools" "perfsonar-testpoint" "perfsonar-core" "perfsonar-centralmanagement" "perfsonar-toolkit")
+declare -a BUNDLES=("perfsonar-tools" "perfsonar-testpoint" "perfsonar-core" "perfsonar-centralmanagement" "perfsonar-toolkit")
 TEXT_STATUS=""
 docker-compose down
-for REPO in ${REPOS[@]}; do
-    echo "BUILD REPO $REPO"
-    docker-compose build --no-cache --force-rm --build-arg repo="$REPO"  centos_clean
+for BUNDLE in ${BUNDLES[@]}; do
+    echo "BUILD BUNDLE $BUNDLE"
+    docker-compose build --no-cache --force-rm --build-arg repo="$BUNDLE"  centos_clean
     STATUS=$?
-    echo "$REPO Tried to build; status: $STATUS"
+    echo "$BUNDLE Tried to build; status: $STATUS"
     if [ "$STATUS" -eq "0" ]; then
-        echo "$REPO SUCCEDED!"
-        TEXT_STATUS+="$REPO SUCCEEDED!\n"
+        echo "$BUNDLE SUCCEDED!"
+        TEXT_STATUS+="$BUNDLE SUCCEEDED!\n"
         #exit;
     else
-        echo "$REPO FAILED!"
-        TEXT_STATUS+="$REPO FAILED!\n"
+        echo "$BUNDLE FAILED!"
+        TEXT_STATUS+="$BUNDLE FAILED!\n"
     fi
     docker-compose down
 
