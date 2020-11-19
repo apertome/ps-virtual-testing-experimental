171515d12c5c380f921215196bbbeab11949b2c7
Michael Johnson <mj82@globalnoc.iu.edu>
2020-11-19 01:43:46 +0000

    Add repo URLs, integrate bundles

--- docker/testpoint-rpm-install/test_install_instructions.sh
+++ docker/testpoint-rpm-install/test_install_instructions.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/bash
 
-declare -a BUNDLES=("perfsonar-tools" "perfsonar-testpoint" "perfsonar-core" "perfsonar-centralmanagement" "perfsonar-toolkit")
+# default repo
+REPO="nightly-minor"
+
+# set repo if commandline argument is set
+if [ -n "$1" ]; then
+    REPO=$1
+fi
+
+echo "REPO: $REPO"
+
+#declare -a BUNDLES=("perfsonar-tools" "perfsonar-testpoint" "perfsonar-core" "perfsonar-centralmanagement" "perfsonar-toolkit")
+declare -a BUNDLES=("perfsonar-psconfig-web-admin-ui perfsonar-psconfig-web-admin-publisher" "perfsonar-tools" "perfsonar-testpoint" "perfsonar-core" "perfsonar-centralmanagement" "perfsonar-toolkit")
 TEXT_STATUS=""
 docker-compose down
+docker-compose build --no-cache --force-rm --build-arg bundle="$BUNDLE"  centos_clean
 for BUNDLE in ${BUNDLES[@]}; do
     echo "BUILD BUNDLE $BUNDLE"
-    docker-compose build --no-cache --force-rm --build-arg repo="$BUNDLE"  centos_clean
+    docker-compose up -d
+    docker-compose exec centos_clean /usr/bin/ps_install_bundle.sh "$BUNDLE" "$REPO"
+
     STATUS=$?
     echo "$BUNDLE Tried to build; status: $STATUS"
     if [ "$STATUS" -eq "0" ]; then
